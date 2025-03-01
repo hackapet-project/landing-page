1ad30c7ed4090c84503829b47e06f7b73eea723f
Ignacio Delgado <me@igdel.com>
2025-03-01 18:44:53 +0100

    fix: motion

--- components/motion.tsx
+++ components/motion.tsx
@@ -1,19 +1,43 @@
 "use client";
 
 import { HTMLMotionProps, motion as framerMotion } from "framer-motion";
+import React from "react";
 
-type MotionDivProps = HTMLMotionProps<"div">;
+// Use the correct React types for the ref
+type MotionDivProps = HTMLMotionProps<"div"> & React.RefAttributes<HTMLDivElement>;
 
 export const motion = {
-  div: (props: MotionDivProps) => <framerMotion.div {...props} />,
-  h1: (props: HTMLMotionProps<"h1">) => <framerMotion.h1 {...props} />,
-  h2: (props: HTMLMotionProps<"h2">) => <framerMotion.h2 {...props} />,
-  h3: (props: HTMLMotionProps<"h3">) => <framerMotion.h3 {...props} />,
-  p: (props: HTMLMotionProps<"p">) => <framerMotion.p {...props} />,
-  span: (props: HTMLMotionProps<"span">) => <framerMotion.span {...props} />,
-  a: (props: HTMLMotionProps<"a">) => <framerMotion.a {...props} />,
-  ul: (props: HTMLMotionProps<"ul">) => <framerMotion.ul {...props} />,
-  li: (props: HTMLMotionProps<"li">) => <framerMotion.li {...props} />,
-  button: (props: HTMLMotionProps<"button">) => <framerMotion.button {...props} />,
-  section: (props: HTMLMotionProps<"section">) => <framerMotion.section {...props} />,
+  div: React.forwardRef<HTMLDivElement, HTMLMotionProps<"div">>((props, ref) =>
+      <framerMotion.div {...props} ref={ref} />
+  ),
+  h1: React.forwardRef<HTMLHeadingElement, HTMLMotionProps<"h1">>((props, ref) =>
+      <framerMotion.h1 {...props} ref={ref} />
+  ),
+  h2: React.forwardRef<HTMLHeadingElement, HTMLMotionProps<"h2">>((props, ref) =>
+      <framerMotion.h2 {...props} ref={ref} />
+  ),
+  h3: React.forwardRef<HTMLHeadingElement, HTMLMotionProps<"h3">>((props, ref) =>
+      <framerMotion.h3 {...props} ref={ref} />
+  ),
+  p: React.forwardRef<HTMLParagraphElement, HTMLMotionProps<"p">>((props, ref) =>
+      <framerMotion.p {...props} ref={ref} />
+  ),
+  span: React.forwardRef<HTMLSpanElement, HTMLMotionProps<"span">>((props, ref) =>
+      <framerMotion.span {...props} ref={ref} />
+  ),
+  a: React.forwardRef<HTMLAnchorElement, HTMLMotionProps<"a">>((props, ref) =>
+      <framerMotion.a {...props} ref={ref} />
+  ),
+  ul: React.forwardRef<HTMLUListElement, HTMLMotionProps<"ul">>((props, ref) =>
+      <framerMotion.ul {...props} ref={ref} />
+  ),
+  li: React.forwardRef<HTMLLIElement, HTMLMotionProps<"li">>((props, ref) =>
+      <framerMotion.li {...props} ref={ref} />
+  ),
+  button: React.forwardRef<HTMLButtonElement, HTMLMotionProps<"button">>((props, ref) =>
+      <framerMotion.button {...props} ref={ref} />
+  ),
+  section: React.forwardRef<HTMLElement, HTMLMotionProps<"section">>((props, ref) =>
+      <framerMotion.section {...props} ref={ref} />
+  ),
 };
